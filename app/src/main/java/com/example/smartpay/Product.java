7f1075cf4f3eeb2a9ca079436b9a9e2dbddda93a
hiren <hirendesai333@gmail.com>
2020-04-17 20:13:27 +0530

    Fix the bug.

---
+++ app/src/main/java/com/example/smartpay/Product.java
@@ -0,0 +1,80 @@
+package com.example.smartpay;
+
+public class Product {
+    private String ItemName;
+    private String Price;
+    private String Weight;
+    private String Qty;
+    private String TotalPrice;
+    private String ItemImage;
+//    private Double PaymentRefNo;
+
+    public Product() {
+    }
+
+    public Product(String itemName, String price, String weight, String qty, String totalPrice, String itemImage, Double paymentRefNo) {
+        ItemName = itemName;
+        Price = price;
+        Weight = weight;
+        Qty = qty;
+        TotalPrice = totalPrice;
+        ItemImage = itemImage;
+//        PaymentRefNo = paymentRefNo;
+    }
+
+    public String getItemName() {
+        return ItemName;
+    }
+
+    public void setItemName(String itemName) {
+        ItemName = itemName;
+    }
+
+    public String getPrice() {
+        return Price;
+    }
+
+    public void setPrice(String price) {
+        Price = price;
+    }
+
+    public String getWeight() {
+        return Weight;
+    }
+
+    public void setWeight(String weight) {
+        Weight = weight;
+    }
+
+    public String getQty() {
+        return Qty;
+    }
+
+    public void setQty(String qty) {
+        Qty = qty;
+    }
+
+    public String getTotalPrice() {
+        return TotalPrice;
+    }
+
+    public void setTotalPrice(String totalPrice) {
+        TotalPrice = totalPrice;
+    }
+
+    public String getItemImage() {
+        return ItemImage;
+    }
+
+    public void setItemImage(String itemImage) {
+        ItemImage = itemImage;
+    }
+/*
+    public Double getPaymentRefNo() {
+        return(PaymentRefNo);
+    }
+
+    public void setPaymentRefNo(Double paymentRefNo) {
+        PaymentRefNo = paymentRefNo;
+    }*/
+}
--- app/src/main/java/com/example/smartpay/TransactionActivity.java
+++ app/src/main/java/com/example/smartpay/TransactionActivity.java
@@ -2,6 +2,8 @@ package com.example.smartpay;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,10 +13,12 @@ import android.widget.TextView;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.Date;
+import java.util.List;
 
 public class TransactionActivity extends AppCompatActivity {
 
     TextView dateTime;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +38,30 @@ public class TransactionActivity extends AppCompatActivity {
                         (LinearLayout)findViewById(R.id.bottomsheet));
                 bottomSheetDialog.setContentView(view);
                 bottomSheetDialog.show();
+            }
+        });
+
+        recyclerView = findViewById(R.id.recyclerview_products);
+        new FirebaseDatabaseHelper().ReadProducts(new FirebaseDatabaseHelper.DataStatus() {
+            @Override
+            public void DataIsLoaded(List<Product> products, List<String> keys) {
+                new RecyclerView_Config().setConfig(recyclerView,TransactionActivity.this, products, keys);
+
+            }
+
+            @Override
+            public void DataIsInserted() {
+
+            }
+
+            @Override
+            public void DataIsUpdated() {
+
+            }
+
+            @Override
+            public void DataIsDeleted() {
+
             }
         });
 
